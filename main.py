4cfff23f4af0b951417d14ec9e72be592d5983a2
★. KuJu -TH- <plan291037@outlook.com>
2022-06-03 15:20:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 from __future__ import print_function
-import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
-
-from google.oauth2 import service_account
-# import multiprocessing
 from pathlib import Path
 from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
+
 import io
-import sys
+import os
+import json
+
+CREDS_FILE_LOCATION = os.path.join(os.getcwd(),"sa-account.json")
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = CREDS_FILE_LOCATION
+
+from google.cloud import vision
+
+with open('config.json') as config_file:
+    data = json.load(config_file)
 
 SCOPES = ['https://www.googleapis.com/auth/drive']
+CLIENT_ACCOUNT = os.path.isfile('./credentials.json')
 CLIENT_SECRET_FILE = 'credentials.json'
 
-SA_ACCOUNT = os.path.isfile('./sa-account.json')
-SA_SECRET_FILE = 'sa-account.json'
 
 def get_credentials(CLIENT_SECRET_FILE):
     creds = None
@@ -40,106 +45,154 @@ def get_credentials(CLIENT_SECRET_FILE):
     return build('drive', 'v3', credentials=creds)
 
 
-def Create_Sa_Service(SA_SECRET_FILE):
-    credentials = service_account.Credentials.from_service_account_file(SA_SECRET_FILE,scopes=SCOPES)
-    return build('drive', 'v3', credentials=credentials, cache_discovery=False)
+def detect_text(path):
+    """Detects text in the file."""
+    client = vision.ImageAnnotatorClient()
 
-def main(mydir="images"):
-    if SA_ACCOUNT and SA_SECRET_FILE:
-        service = Create_Sa_Service(SA_SECRET_FILE)
-    else:
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+
+    retval = None
+    if len(texts) > 0:
+        retval = texts[0].description
+        # retval += '\n'    
+    
+    return retval
+
+
+def main(mydir=data['Image_location']):
+    if CLIENT_ACCOUNT and CLIENT_SECRET_FILE:
         service = get_credentials(CLIENT_SECRET_FILE)
-    # imgfile = 'image.jpeg'  # Image with texts (png, jpg, bmp, gif, pdf)
-    # txtfile = 'text.txt'  # Text file outputted by OCR
+        
+        current_directory = Path(Path.cwd())
+        raw_texts_dir = Path(f'{current_directory}/raw_texts')
+        texts_dir = Path(f'{current_directory}/texts')
+        srt_file = open(Path(f'{current_directory}/subtitle_output.srt'), 'a', encoding='utf-8')
+        line = 1
 
-    current_directory = Path(Path.cwd())
-    images_dir = Path(f'{current_directory}/{mydir}')
-    raw_texts_dir = Path(f'{current_directory}/raw_texts')
-    texts_dir = Path(f'{current_directory}/texts')
-    srt_file = open(Path(f'{current_directory}/subtitle_output.srt'), 'a', encoding='utf-8')
-    line = 1
+        if not raw_texts_dir.exists():
+            raw_texts_dir.mkdir()
+        if not texts_dir.exists():
+            texts_dir.mkdir()
 
-    # check directory if exists
-    if not images_dir.exists():
-        images_dir.mkdir()
-        print('Images folder is empty.')
-        exit()
+        images = Path(f'{mydir}').rglob('*.jpeg')
+        for image in images:
 
-    if not raw_texts_dir.exists():
-        raw_texts_dir.mkdir()
-    if not texts_dir.exists():
-        texts_dir.mkdir()
+            # Get data
+            imgfile = str(image.absolute())
+            imgname = str(image.name)
+            raw_txtfile = f'{current_directory}/raw_texts/{imgname[:-5]}.txt'
+            txtfile = f'{current_directory}/texts/{imgname[:-5]}.txt'
 
-    images = Path(f'{current_directory}/{mydir}').rglob('*.jpeg')
-    for image in images:
+            if os.path.exists(txtfile):
+                continue
 
-        # Get data
-        imgfile = str(image.absolute())
-        imgname = str(image.name)
-        raw_txtfile = f'{current_directory}/raw_texts/{imgname[:-5]}.txt'
-        txtfile = f'{current_directory}/texts/{imgname[:-5]}.txt'
+            mime = 'application/vnd.google-apps.document'
+            res = service.files().create(
+                body={
+                    'name': imgname,
+                    'mimeType': mime
+                },
+                media_body=MediaFileUpload(imgfile, mimetype=mime, resumable=True)
+            ).execute()
 
-        if os.path.exists(txtfile):
-            continue
+            downloader = MediaIoBaseDownload(
+                io.FileIO(raw_txtfile, 'wb'),
+                service.files().export_media(fileId=res['id'], mimeType="text/plain")
+            )
+            done = False
+            while done is False:
+                status, done = downloader.next_chunk()
 
-        mime = 'application/vnd.google-apps.document'
-        res = service.files().create(
-            body={
-                'name': imgname,
-                'mimeType': mime
-            },
-            media_body=MediaFileUpload(imgfile, mimetype=mime, resumable=True)
-        ).execute()
+            service.files().delete(fileId=res['id']).execute()
 
-        downloader = MediaIoBaseDownload(
-            io.FileIO(raw_txtfile, 'wb'),
-            service.files().export_media(fileId=res['id'], mimeType="text/plain")
-        )
-        done = False
-        while done is False:
-            status, done = downloader.next_chunk()
+            # Create clean text file
+            raw_text_file = open(raw_txtfile, 'r', encoding='utf-8')
+            text_content = raw_text_file.read()
+            raw_text_file.close()
+            text_content = text_content.split('\n')
+            text_content = ''.join(text_content[2:])
+            text_file = open(txtfile, 'w', encoding='utf-8')
+            text_file.write(text_content)
+            text_file.close()
 
-        service.files().delete(fileId=res['id']).execute()
+            start_hour = imgname.split('_')[0][:2]
+            start_min = imgname.split('_')[1][:2]
+            start_sec = imgname.split('_')[2][:2]
+            start_micro = imgname.split('_')[3][:3]
 
-        # Create clean text file
-        raw_text_file = open(raw_txtfile, 'r', encoding='utf-8')
-        text_content = raw_text_file.read()
-        raw_text_file.close()
-        text_content = text_content.split('\n')
-        text_content = ''.join(text_content[2:])
-        text_file = open(txtfile, 'w', encoding='utf-8')
-        text_file.write(text_content)
-        text_file.close()
+            end_hour = imgname.split('__')[1].split('_')[0][:2]
+            end_min = imgname.split('__')[1].split('_')[1][:2]
+            end_sec = imgname.split('__')[1].split('_')[2][:2]
+            end_micro = imgname.split('__')[1].split('_')[3][:3]
 
-        start_hour = imgname.split('_')[0][:2]
-        start_min = imgname.split('_')[1][:2]
-        start_sec = imgname.split('_')[2][:2]
-        start_micro = imgname.split('_')[3][:3]
+            # Format start time
+            start_time = f'{start_hour}:{start_min}:{start_sec},{start_micro}'
 
-        end_hour = imgname.split('__')[1].split('_')[0][:2]
-        end_min = imgname.split('__')[1].split('_')[1][:2]
-        end_sec = imgname.split('__')[1].split('_')[2][:2]
-        end_micro = imgname.split('__')[1].split('_')[3][:3]
+            # Format end time
+            end_time = f'{end_hour}:{end_min}:{end_sec},{end_micro}'
+            # Append the line to srt file
+            srt_file.writelines([
+                f'{line}\n',
+                f'{start_time} --> {end_time}\n',
+                f'{text_content}\n\n',
+                ''
+            ])
+            print(f"""{line}: [ {start_time} ]: {imgname}\n{text_content}\n""")
+            line += 1
 
-        # Format start time
-        start_time = f'{start_hour}:{start_min}:{start_sec},{start_micro}'
+        srt_file.close()
+    else:
+        current_directory = os.getcwd()
+        srt_file = open(os.path.join(f'{current_directory}','subtitle_output.srt'), 'w', encoding='utf-8')
 
-        # Format end time
-        end_time = f'{end_hour}:{end_min}:{end_sec},{end_micro}'
-        # Append the line to srt file
-        srt_file.writelines([
-            f'{line}\n',
-            f'{start_time} --> {end_time}\n',
-            f'{text_content}\n\n',
-            ''
-        ])
+        images = Path(f'{mydir}').rglob('*.jpeg')
+        line = 1
 
-        line += 1
-        print(f"{line} {imgname} Done.")
+        for image in images:
+            # Get data
+            imgname = str(image.name)
+            text_content = detect_text(image)
 
+            if text_content is not None:
+                text_content = text_content.strip()
+                start_hour = imgname.split('_')[0][:2]
+                start_min = imgname.split('_')[1][:2]
+                start_sec = imgname.split('_')[2][:2]
+                start_micro = imgname.split('_')[3][:3]
 
-    srt_file.close()
+                end_hour = imgname.split('__')[1].split('_')[0][:2]
+                end_min = imgname.split('__')[1].split('_')[1][:2]
+                end_sec = imgname.split('__')[1].split('_')[2][:2]
+                end_micro = imgname.split('__')[1].split('_')[3][:3]
 
+                # Format start time
+                start_time = f'{start_hour}:{start_min}:{start_sec},{start_micro}'
+
+                # Format end time
+                end_time = f'{end_hour}:{end_min}:{end_sec},{end_micro}'
+
+                # Append the line to srt file
+                srt_file.writelines([
+                    f'{line}\n',
+                    f'{start_time} --> {end_time}\n',
+                    f'{text_content}\n\n'
+                ])
+                print(f"""{line}: [ {start_time}] : {imgname}\n{text_content}\n""")
+                line += 1
+
+        srt_file.close()       
 
 if __name__ == '__main__':
    main()
